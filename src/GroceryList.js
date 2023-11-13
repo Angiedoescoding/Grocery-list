b6f9bf8bf5f0bd6b5cd0ff35d99e34d153311121
Angiedoescoding <139031564+Angiedoescoding@users.noreply.github.com>
2023-11-12 21:28:01 -0800

    Update GroceryList.js

--- src/GroceryList.js
+++ src/GroceryList.js
@@ -6,44 +6,40 @@ export class GroceryList extends Component {
     super();
 
         this.state = {
-            userInput: "", // #3  input - это то, что вводит пользователь и что буджет потом меняться. Изначально ставим пустым.
-                        // мы также должны не только следить за тем, что меняется в тексте, но также и что добавляется юзером в какой-то список
-            GroceryList: []    // #4  наш список будет добавляться через массив (один из методов массива), где идут перечни элементов. Это один спрошной массив (array)
+            userInput: "", 
+            GroceryList: []   
         }
     }
 
-onChangeEvent(event) {                  // #2  применяем this.setState чтобы отразить изменение состояния
-    this.setState({userInput: event})   // мы хотим отразить то, что ввел юзер (userInput) и делаем это через event, основу которого мы заложили в стрелочной функции в п. #1
+onChangeEvent(event) {                  
+    this.setState({userInput: event})   
 }
 
-addItem(input) {                 // input - назвать можно как угодно
+addItem(input) {               
     if (input === '') {
         alert ("Please enter an item name")
     } else {                          
-    let listArray = this.state.GroceryList;   // #8 создаем функцию метод addItem и далее указываем переменную, КУДА каждый новый элемент будет помещаться - this.state.GroceryList
-    listArray.push(input)               // #9  прописываем как мы добавляем элементы в список listArray - через метод джаваскрипт - .push() - он ставит каждый 
-                                        // новый элемент, добавленный пользователем, в конец списка
-    this.setState({GroceryList: listArray, userInput: ''}) // #10 приравниваем список продуктов (массив) GroceryList к тому, что вводит пользователь listArray, 
-                                                            // а также то поле ввода юзером userInput к пустоте через пустые кавычки ‘’ - после нажатия на кнопку поле ввода  очистится.
+    let listArray = this.state.GroceryList;  
+    listArray.push(input)               
+    this.setState({GroceryList: listArray, userInput: ''}) 
     console.log(listArray)
     }
 }
 
 
 crossedWord(event) {
-    const li = event.target;                 // #13 - мы добавили event, так как добавили подслушку на элемент списка (задав const и li) и следим за ним (target). 
-                                            // const li может иметь любое имя (const apple, for example) - не будет играть роли, так как onClick (#11) стоит на элементе <li></li>
-    li.classList.toggle('crossed');         // #14 - как только пользователь нажмет на элемент списка (li), создастся класс и с элементом произойдут изменения, указанные в CSS.
+    const li = event.target;        
+    li.classList.toggle('crossed');     
 }
 
 deleteItem() {
-    let listArray = this.state.GroceryList;     // получаем доступ снова к списку продуктов
-    listArray = [];                            // это один из способов JS очистить массив: []; еще можно приравнять length к нулю: listArray.length = 0
+    let listArray = this.state.GroceryList;  
+    listArray = [];                           
     this.setState({GroceryList: listArray})
 }
 
 onFormSubmit(e) {
-     e.preventDefault()         // е - нажатие на Enter; e.preventDefault() - предотвратить перезагрузку страницы каждый раз, когда нажимаем на кнопку Enter.
+     e.preventDefault()      
 }
 
     render () {
@@ -53,9 +49,8 @@ onFormSubmit(e) {
                 <div className="container">             
                 <input type="text" 
                 placeholder="What do you want to buy?"
-                onChange={(event) => {this.onChangeEvent(event.target.value)}}     // #1    стрелочная function with .onChangeEvent() где мы прописываем, что мы
-                                                            // хотим реагировать на изменения (продвинутый способ написания - запомнить) и что #6 мы получаем доступ к тому, что пишет пользователь
-                value={this.state.userInput}                    // #5     value={} - это то, чему равно то, что пишет пользователь на данный момент (то есть значение)
+                onChange={(event) => {this.onChangeEvent(event.target.value)}}   
+                value={this.state.userInput}                   
                 />     
                 </div>
 
@@ -69,8 +64,7 @@ onFormSubmit(e) {
                         <li onClick={this.crossedWord} key={index}>
                         <img src={check} width="30px" alt="checkmark"/>    
                         {item}
-                        </li>    // #11 - item - мы говорим, что мы хотим получить доступ (показать) каждый элемент, так как мы поставили map() на весь список -> 
-                                    // показываем каждый элемент списка. <li></li> в стрелочной функции - мы говорим системе о месте, ГДЕ мы хотим все введеное отразить.
+                        </li>   
                         ))} 
                     </ul>
                 <div className="container">
@@ -82,12 +76,3 @@ onFormSubmit(e) {
         )
     }
 }
-
-
-// onChangeEvent - это наша функция, которую мы затем прописываем выше
-// onChangeEvent(event) {} - где event - это мы хотим следить за событием.  Иногда можнл увидеть просто (е).
-// #7 - кнопка ADD: добавденные предметы берутся из того, что внес юзер - через this.state.userInput и потом мы прописываем новосозданный элемент (метод) addItem как функцию (п. 8)
-// #12   - <li onClick={this.crossedWord} key={index}>{item}</li>  - работаем с перечеркиванием слова, когда юземнанего нажимает - через onClick={this.crossedWord}, где crossedWord - это функция, название которой мы придумали и пропишем ее НАД render (введем класс). onClick - мы говорим системе, что функция должна срабатывать ПРИНАЖАТИИ ПОЛЬЗОВАТЕЛЕМ НА СЛОВО.
-
-// #15 <input> заворачиваем в <form></form>, чтобы иметь возможность добавлять элементы в список продуктов нажатием кнопки Enter: используется атрибут onSubmit, в котором прописывается необходимая команда и далее - метод. 
-// То есть, чтобы кнопка Энтер срабатывала при добавлении элемента в список продуктов, нужно все, что мы напечатали ранее просто обернуть в тег <form></form> и в нем прописать атрибут onSubmit, в котором дальше указать, что системе нужно сделать (через this.имяМетода/функции). Далее уже прописываем детали: 
